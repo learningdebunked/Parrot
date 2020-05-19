85dcdbdf0e02c077132934ac6b0afb49c7d4229c
Kapil <kapz.tcs@gmail.com>
2020-05-18 18:40:03 -0700

    Mock service first commit

---
+++ src/test/java/com/learningdebunked/mock/MockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningdebunked.mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
