ae1583a08f4a248fc01afd01f7ff1ca053ac8dba
Kapil <kapz.tcs@gmail.com>
2020-05-28 23:41:50 -0700

    Code cleanup- part2

--- src/main/java/com/learningdebunked/mock/service/FileWatcherService.java
+++ src/main/java/com/learningdebunked/mock/service/FileWatcherService.java
@@ -32,32 +32,18 @@ public class FileWatcherService {
      */
     public void monitor() {
         try {
-
-            //monitor the files and  call the propogation service
-            //validations can all be aspects
-            System.out.println("***** Directory being monitored is:********" + dirUrl);
             //TODO checkifDirExists , this can be an aspect
-            //TOO another aspect could be if the directory exists and if the mode is dev mode , we need to validate if the files are valid json , if not send an email to the the developer
-            //sending an email can also be an after returning aspect , sending an event , either email or slack notification
-
             if (checkIfFilesExist(dirUrl)) {
                 //TODO decide if u want to use Executors.newWorkStealingPool(10) instead of fixed thread pool
                 ExecutorService pool = Executors.newFixedThreadPool(10);
                 fileProcessor.processFolder(pool, dirUrl);
-                //TODO also need to check if its first time setup , if yes and if the production mode is true we can push these templates to production db
-                //TODO read the existing templates and publish them into the database.
-                //need to lock the directory on the server such that the templates are not modified , otherwise updated templates are not saved or could be saved twice
-                //tODO instantiate multiple threads to read all the files and update them into database
-                //TODO this should happen only if the profile in which the server is running is a dev profile
-                //TODO add profile prod.mode = false
-
+                //TODO to lock the directory on the server such that the templates are not modified , otherwise updated templates are not saved or could be saved twice
                 //Once the initial setup is done , the service should start watching the directory for changes
                 readStream(dirUrl);
-
             }
-
         } catch (IOException | InterruptedException e) {
             //TODO unable to read the directory , may be there are file access issues
+            //TODO Logging
             e.printStackTrace();
         }
     }
